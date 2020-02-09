271ff01cffd8b930cd014877aba44ab9b506994f
Kevenn Caterinque <k.douglascaterinque@gmail.com>
2020-02-09 09:27:48 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+export default function App() {
+  return (
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  )
+}
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+
+function FullName() {
+  const firstName = 'Kevenn';
+  const lastName = 'Caterinque';
+
+  const styles = {
+    color: '#FFAC00',
+    backgroundColor: '#e2e2e2'
+  }
+
+  return (
+    <h1 style={styles}>Hello {firstName + ' ' + lastName}</h1>
+  )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

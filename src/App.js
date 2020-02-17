11fcfc89ef5eae5250f86eb783b64d3a41cf5187
Kevenn Caterinque <k.douglascaterinque@gmail.com>
2020-02-17 08:29:46 -0300

    Learning how to work with events and states

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import TodoItem from './components/TodoItem'
 import todosData from './todosData'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {todos : todosData}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      return {
+        todos : prevState.todos.map(item => {
+          if (item.id === id) item.completed = !item.completed
+          return item
+        })
+      } 
+    })
+  }
+
   render() {
-    const todosComponents = todosData.map(item => <TodoItem key={item.id} item={item} />)
+    const todosComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
     return (
       <div className="todo-list">
         {todosComponents}
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
 
 export default class TodoItem extends Component {
+  constructor() {
+    super()
+    this.state = {checked : false}
+  }
+
 
   render() {
     return (
       <div className="todo-item">
-        <input type="checkbox" checked={this.props.item.completed} />
+        <input type="checkbox" checked={this.props.item.completed} onChange={() => this.props.handleChange(this.props.item.id)} />
         <p>{this.props.item.text}</p>
       </div>
     )
